2a4cb4cb6e2a87c95a86d4e4864b109a23b44636
Austin Brown <austin.nils@gmail.com>
2017-09-24 20:07:32 -0700

    Remove extraneous code during testing. Removed solid flag

--- Write-Block.ps1
+++ Write-Block.ps1
@@ -17,18 +17,13 @@ Function Write-Block {
         [ValidateLength(1,1)]
         [string] $char = "*",
 
-        [switch] $solid, 
         [switch] $noBorder
     )
 
     Begin {
         $lineBreak = "{0}" -f  $char * $width
         
-        if($solid) {
-            $hollowBreak = $lineBreak
-        } else {
-            $hollowBreak = "{0} {1} {2}" -f $char, $(" " * $paddedWidth), $char
-        }
+        $hollowBreak = $lineBreak
 
         # $paddedWidth = $width - ($char.Length * 2 + 2) # 2 spaces are the "padding"
         $paddedWidth = $width - 4 # 2 spaces are the "padding"
@@ -88,12 +83,3 @@ Function Write-Block {
         Write-Host $lineBreak
     }
 }
-
-$messages = @(
-    "$env:COMPUTERNAME - Last Reboot $(Get-Date)",
-    "test this",
-    "and this one too"
-    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-)
-
-Write-Block $messages -char '#'.
